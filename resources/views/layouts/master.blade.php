a7a8145cf928297c8a77208b56533820b965fbc4
MohammadShawon <13203016@iubat.edu>
2017-03-14 21:22:04 +0600

    Install Laravel

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Medicall</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Bootstarp Js -->
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
+    <!-- Custom styles for this template -->
+    <link href="/css/carousel.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+
+
+  </head>
+  <body>
+
+  @include('layouts.nav-home')
+
+  <div class="content">
+       @yield('content')
+   </div> 
+
+
+   @include('layouts.footer')
+  </body>
+</html>
---
+++ resources/views/layouts/nav-home.blade.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse nav-bg">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">Medicall</a>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ 'appointment' }}">Appointment</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Message</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Support</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">FAQ</a>
+          </li>
+
+        </ul>
+        
+        <form class="form-inline mt-2 mt-md-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+        </form> 
+      </div>
+    </nav>
