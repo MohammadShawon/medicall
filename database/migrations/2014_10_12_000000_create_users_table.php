7100295294390ceba4a98f6327c4e1e33ff0f964
MohammadShawon <13203016@iubat.edu>
2017-03-16 22:38:40 +0600

    user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('address');
+            $table->integer('phone');
+            $table->date('dateofbirth');
+            $table->boolean('status');
             $table->rememberToken();
             $table->timestamps();
         });
