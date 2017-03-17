949484528948314e6c2ef90ed1b749372aabc96d
MohammadShawon <13203016@iubat.edu>
2017-03-17 12:23:10 +0600

    sidebar menu add

---
+++ resources/views/users/user-sidebar.blade.php
@@ -0,0 +1,103 @@
+<aside>
+    <div id="sidebar"  class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu">
+            <li class="active">
+                <a class="" href="index.html">
+                    <i class="icon_house_alt"></i>
+                    <span>My Board</span>
+                </a>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_document_alt"></i>
+                    <span>My Info</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <!--<ul class="sub">
+                    <li><a class="" href="form_component.html">Form Elements</a></li>
+                    <li><a class="" href="form_validation.html">Form Validation</a></li>
+                </ul> -->
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_desktop"></i>
+                    <span>Make Appointment</span>
+                    <!-- <span class="menu-arrow arrow_carrot-right"></span> -->
+                </a>
+               <!-- <ul class="sub">
+                    <li><a class="" href="general.html">Components</a></li>
+                    <li><a class="" href="buttons.html">Buttons</a></li>
+                    <li><a class="" href="grids.html">Grids</a></li>
+                </ul>
+                -->
+            </li>
+            <li>
+                <a class="" href="#">
+                    <i class="icon_genius"></i>
+                    <span>Prescription</span>
+                </a>
+            </li>
+            <li>
+                <a class="" href="#">
+                    <i class="icon_piechart"></i>
+                    <span>Ask Your Question</span>
+
+                </a>
+
+            </li>
+
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_table"></i>
+                    <span>Credits</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+
+            </li>
+
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_documents_alt"></i>
+                    <span>Share</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <ul class="sub">
+                    <li><a class="" href="#">Facebook</a></li>
+                    <li><a class="" href="#"><span>Google Plus</span></a></li>
+                    <li><a class="" href="#">Linked In</a></li>
+                    <li><a class="" href="#">Twitter</a></li>
+                </ul>
+            </li>
+
+            li class="sub-menu">
+            <a href="javascript:;" class="">
+                <i class="icon_table"></i>
+                <span>About Us</span>
+
+            </a>
+
+            </li>
+
+            li class="sub-menu">
+            <a href="javascript:;" class="">
+                <i class="icon_table"></i>
+                <span>Terms & Condition</span>
+
+            </a>
+
+            </li>
+
+            li class="sub-menu">
+            <a href="javascript:;" class="">
+                <i class="icon_table"></i>
+                <span>Logout</span>
+
+            </a>
+
+            </li>
+
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
